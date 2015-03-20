98eb3899915ffce827a649a590ea224fb71a493a
anthonysalembier <asalembier@excilys.com>
2015-03-20 18:06:05 +0100

    balbla

---
+++ src/main/java/com/excilys/controller/HelloServlet.java
@@ -0,0 +1,30 @@
+package com.excilys.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+	
+	public HelloServlet() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.getServletContext().getRequestDispatcher("/WEB-INF/views/index.jsp").forward(request, response);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
