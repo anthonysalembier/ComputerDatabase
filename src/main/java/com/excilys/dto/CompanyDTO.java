f2289578fdfd5246ad9913c947937585433dbae9
anthonysalembier <asalembier@excilys.com>
2015-03-25 12:04:06 +0100

    DTO computer et company

---
+++ src/main/java/com/excilys/dto/CompanyDTO.java
@@ -0,0 +1,58 @@
+package com.excilys.dto;
+
+public class CompanyDTO {
+	
+	private String id;
+	private String name;
+	
+	public CompanyDTO() {
+		
+	}
+	
+	public CompanyDTO(String id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CompanyDTO other = (CompanyDTO) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+}
